72b01bbdd68acbfee3315348e04d7373af218360
MyrzakhmetovTemirlan <temirlanmirzakhmetov@gmail.com>
2019-06-25 11:21:02 +0900

    Add login route

--- routes.py
+++ routes.py
@@ -1,7 +1,9 @@
 from chat.views import GroupGeneral, GroupInterns, WebSocket
+from auth.views import Login
 
 routes = [
 	('GET', '/', GroupGeneral, 'general'),
 	('GET', '/interns', GroupInterns, 'interns'),
 	('GET', '/ws', WebSocket, 'chat'),
+	('GET', '/login', Login, 'login')
 ]
