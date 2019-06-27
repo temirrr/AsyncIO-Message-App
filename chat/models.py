a54a1397e082998f567e9184dea1b6f9623e32c2
MyrzakhmetovTemirlan <temirlanmirzakhmetov@gmail.com>
2019-06-27 16:01:57 +0900

    Add initial setup for db

---
+++ chat/models.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+import aiopg
+
+class Message():
+	def __init__(self, **kwargs):
+		self.db_str = 'dbname=postgres user=postgres password=timkabro7 host=127.0.0.1 port=5002'
+
+	async def get_messages(self, table_name):
+	    pool = await aiopg.create_pool(self.db_str)
+	    async with pool.acquire() as conn:
+	        async with conn.cursor() as cur:
+	            await cur.execute(f"SELECT messages FROM {table_name};")
+	            ret = {'messages': []}
+	            async for row in cur:
+	                ret['messages'].append(row[0])
+	            return ret
+	            
+
+
+	'''loop = asyncio.get_event_loop()
+	loop.run_until_complete(go())'''
+
+	async def send_message(self, table_name, message):
+		pool = await aiopg.create_pool(self.db_str)
+		async with pool.acquire() as conn:
+			async with conn.cursor() as cur:
+				await cur.execute(f"INSERT INTO {table_name} (messages) VALUES ('{message}');")
+
+
+
+
--- chat/views.py
+++ chat/views.py
@@ -1,16 +1,25 @@
 import aiohttp_jinja2
 from aiohttp import web, WSMsgType
 
+import aiopg
+import asyncio
+
 from aiohttp_session import get_session
 
 from settings import log
+
+from chat.models import Message
 
 import random
 
 class GroupGeneral(web.View):
 	@aiohttp_jinja2.template('groups/general.html')
 	async def get(self):
-		return {}
+		message = Message()
+		message_list = await message.get_messages('group_general')
+		log.debug(f'messages are: {message_list}')
+		messages = message_list
+		return messages
 
 class GroupInterns(web.View):
 	@aiohttp_jinja2.template('groups/interns.html')
@@ -19,39 +28,51 @@ class GroupInterns(web.View):
 
 class WebSocketGeneral(web.View):
 	async def get(self):
+		db_str = 'dbname=postgres user=postgres password=timkabro7 host=127.0.0.1 port=5002'
+
 		ws = web.WebSocketResponse()
 		await ws.prepare(self.request)
 
 		session = await get_session(self.request)
 		uid = session.get('uid')
 
-		#broadcast joining of new user
-		for _ws in self.request.app['websockets_general']:
-			await _ws.send_str('"%s" has joined the chat' % (uid))
-			log.debug('"%s" has joined the chat' % (uid))
-		self.request.app['websockets_general'].append(ws)
+		pool = await aiopg.create_pool(db_str)
+		async with pool.acquire() as conn:
+			async with conn.cursor() as cur:
+				#broadcast joining of new user
+				join = ('"%s has joined the chat"' % (uid))
+				json_join = '{{"msg": {0}}}'.format(join)
+				await cur.execute("INSERT INTO group_general (messages) VALUES ('{0}');".format(json_join))
 
-		#send client's id to this particular client for frontend
-		log.debug('before sending UID')
-		await ws.send_str('{"myID": "%s"}' % (uid))
-		log.debug('after sending UID')
+				for _ws in self.request.app['websockets_general']:
+					await _ws.send_str(join)
+				self.request.app['websockets_general'].append(ws)
 
-		async for msg in ws:
-			if msg.type == WSMsgType.TEXT:
-				if msg.data == 'exit-chat':
-					await ws.close()
-				else:
-					for _ws in self.request.app['websockets_general']:
-						await _ws.send_str('{"user": "%s", "msg": "%s"}' % (uid, msg.data))
-			elif msg.type == WSMsgType.ERROR:
-				log.debug('ws connection closed with exception {0}'.format(ws.exception()))
+				#send client's id to this particular client for frontend
+				log.debug('before sending UID')
+				await ws.send_str('{"myID": "%s"}' % (uid))
+				log.debug('after sending UID')
 
-		self.request.app['websockets_general'].remove(ws)
-		for _ws in self.request.app['websockets_general']:
-			await _ws.send_str('"%s" has left the chat' % (uid))
-		log.debug('websocket connection closed')
+				async for msg in ws:
+					if msg.type == WSMsgType.TEXT:
+						if msg.data == 'exit-chat':
+							await ws.close()
+						else:
+							for _ws in self.request.app['websockets_general']:
+								await _ws.send_str('{"user": "%s", "msg": "%s"}' % (uid, msg.data))
+					elif msg.type == WSMsgType.ERROR:
+						log.debug('ws connection closed with exception {0}'.format(ws.exception()))
 
-		return ws
+				self.request.app['websockets_general'].remove(ws)
+				leave = ('"%s has left the chat"' % (uid))
+				json_leave = '{{"from_user": "yes", "msg": {0}}}'.format(leave)
+				await cur.execute("INSERT INTO group_general (messages) VALUES ('{0}');".format(json_leave))
+
+				for _ws in self.request.app['websockets_general']:
+					await _ws.send_str('"%s" has left the chat' % (uid))
+				log.debug('websocket connection closed')
+
+				return ws
 
 class WebSocketInterns(web.View):
 	async def get(self):
